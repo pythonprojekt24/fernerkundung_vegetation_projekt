a1b1b00b0f46e3e0b4f165e12a13dfe07063289f
Johanna135 <johanna.mascher@student.uibk.ac.at>
2024-06-18 18:14:51 +0200

    l

--- code/srtm_dem_via_eathengine.py
+++ code/srtm_dem_via_eathengine.py
@@ -2,7 +2,8 @@ import ee
 import geemap
 import webbrowser
 
-# Initialize the Earth Engine module.
+# Authenticate and initialize the Earth Engine module.
+ee.Authenticate()
 ee.Initialize()
 
 # Load the SRTM dataset.
@@ -20,9 +21,10 @@ m.set_center(-112.8598, 36.2841, 10)
 m.add_layer(slope, {'min': 0, 'max': 60}, 'slope')
 
 # Save the map to an HTML file.
-map_file = 'slope_map.html'
+map_file = 'plots\slope_map.html'
 m.save(map_file)
 
 # Open the map in the default web browser.
 webbrowser.open(map_file)
+
 
