106b01b1b6ed537119d41d335145ba84753e4110
Johanna135 <johanna.mascher@student.uibk.ac.at>
2024-06-20 10:27:32 +0200

    zugriff mit google earth engine und html karte erstellt

--- code/srtm_dem_via_eathengine.py
+++ code/srtm_dem_via_eathengine.py
@@ -1,12 +1,14 @@
-# einfügen von Geländemodell durch die google earth engine, funktioniert noch nicht aber hoffentlich bald
-
 import ee
 import geemap
 import webbrowser
+import os
 
-# Authenticate and initialize the Earth Engine module.
+# Replace 'your-project-id' with your actual project ID
+project_id = 'ee-mascherjo'
+
+# Authenticate and initialize the Earth Engine module with the project ID.
 ee.Authenticate()
-ee.Initialize()
+ee.Initialize(project=project_id)
 
 # Load the SRTM dataset.
 dataset = ee.Image('CGIAR/SRTM90_V4')
@@ -23,10 +25,10 @@ m.set_center(-112.8598, 36.2841, 10)
 m.add_layer(slope, {'min': 0, 'max': 60}, 'slope')
 
 # Save the map to an HTML file.
-map_file = 'plots\slope_map.html'
+map_file = os.path.join('plots', 'slope_map.html')
+if not os.path.exists('plots'):
+    os.makedirs('plots')
 m.save(map_file)
 
 # Open the map in the default web browser.
-webbrowser.open(map_file)
-
-
+webbrowser.open('file://' + os.path.realpath(map_file))
